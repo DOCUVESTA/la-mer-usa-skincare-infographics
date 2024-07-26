b5ace19f3cda1b0aed74dac9eaa327bf15e53524
Michele Escano <164587076+DOCUVESTA@users.noreply.github.com>
2024-07-25 21:10:28 -0400

    Delete scripts directory

--- scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-from .load_clean_dataframe import LaMerDataLoader
-from .prepare_infographics_data import LaMerDataInfographicsItems
-from .create_graph import LaMerAgeDistributionGraph
-from .generate_pdf import CustomPDF
-
-__all__ = ["LaMerDataLoader", "LaMerDataInfographicsItems", "LaMerAgeDistributionGraph", "CustomPDF"]
